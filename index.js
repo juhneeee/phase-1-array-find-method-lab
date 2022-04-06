66810bb953e45c873404cfc0ebca63463809c29e
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 21:38:12 -0400

    commit tree 3ba14b9572a5c3360df0dc014b51cf2a478236b7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function checkWin(element){ 
+    return (element['result'] === 'W');
+}
+function superbowlWin(arr){
+    let obj = arr.find(checkWin);
+    return obj == undefined ? undefined : obj['year']
+}
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+// const record2 = [ 
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"} ]
